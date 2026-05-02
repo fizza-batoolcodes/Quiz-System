b6117602087b0df122ecffe032c3d28e032fc458
Fizza Batool <fizzabatool.bssef25@iba-suk.edu.pk>
2026-05-02 19:04:15 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
+import application.DBConnection;
+import application.LoginApp;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,16 +8,16 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        System.out.println("MAIN STARTED");
-
-        Label label = new Label("APP IS RUNNING ✔");
-        Scene scene = new Scene(new StackPane(label), 300, 200);
-
-        stage.setScene(scene);
-        stage.show();
+        LoginApp login = new LoginApp();
+        login.start(stage);
     }
 
     public static void main(String[] args) {
+
+        // Check database connection first
+        DBConnection.getConnection();
+
+        // Launch JavaFX app
         launch(args);
     }
 }
